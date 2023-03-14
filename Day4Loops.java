e0abc05c53401c79bbef0dd7f0a69637e6482d35
AllwinJWork <123567122+AllwinJWork@users.noreply.github.com>
2023-03-14 12:58:33 +0000

    Create Day4Loops.java

---
+++ Day4Loops.java
@@ -0,0 +1,27 @@
+package enhancedforloops;
+
+import java.util.ArrayList;
+
+public class Enhanced {
+	public static void main(String[] args) {
+		ArrayList<String> transport = new ArrayList<String>();
+
+		transport.add("Car");
+		transport.add("Bike");
+		transport.add("Train");
+		transport.add("Bus");
+
+		System.out.println(transport);
+
+		for (String transporation : transport) {
+			System.out.println(transporation);
+
+		}
+
+	}
+
+    
+  
+    		
+    }
+
